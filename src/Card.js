7014440771dab44194a6ce119db3c165a1c3e0a7
andreykorolevich <korolevich1994@mail.ru>
2020-12-07 10:35:35 +0300

    first commit

---
+++ src/Card.js
@@ -0,0 +1,12 @@
+function Card({img, discription, children}) {
+    return (
+        <div className="card">
+            {img && <img src={img} className="card-img-top" alt={discription}/>}
+            <div className="card-body">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default Card;
